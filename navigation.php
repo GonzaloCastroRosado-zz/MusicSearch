f063429869ee8f940da60e990f7fb453353ae68a
GonzaloOmmi <gonzalo@ommi.com>
2016-10-27 15:51:03 +0200

    modify navigation.php

--- navigation.php
+++ navigation.php
@@ -1,23 +1,14 @@
 <?php
 
 $dir = "/usr/local/var/www/music/blind_guardian/";
-$s = $_GET['s'];
 
-if(preg_match("/^\s*$/",$s)==1) {
-	exit;
-}
+$command = '/usr/bin/find -H '.$dir.' -type f -name "*.mp3" -iregex ".*.*"';
 
-$s = preg_split("/\ /",$s);
-$command = '/usr/bin/find -H '.$dir.' -type f -name "*.mp3" -iregex ".*'.$s[0].'.*"';
-
-for($i=1;$i<count($s);$i++) {
-	$command .= ' | grep -i "'.$s[$i].'"';
-}
 exec($command.' | head -n100', $output);
 sort($output, SORT_NATURAL);
 
 for($i=0;$i<count($output);$i++) {
-	$output[$i] = str_replace($dir,"http://localhost/music/",$output[$i]);
+	$output[$i] = str_replace($dir,"",$output[$i]);
 }
 
 $response = array("data"=>$output);
